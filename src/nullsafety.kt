18a22a8c20ea8a3d50bd84be8378cf48ef745aae
jwtiyar <jwtiyar@gmail.com>
2017-07-28 22:54:50 +0300

    more

--- src/nullsafety.kt
+++ src/nullsafety.kt
@@ -1,7 +1,7 @@
 fun main(args: Array<String>) {
     var name:String? // am basha abe hemay ? dabney la dway string agina null print nakat .
-    name="h" // agar la jey ama null bnwsy w dw 3alamaty sarswrrman dabney la print awa run naka awaste .
-    println(name!!) // lera aw dw 3alamaty sarswrrmana ka datna wata abe variable ka value y habe agar na yaksar awaste bot run naka .
+    name="3aba" // agar la jey ama null bnwsy w dw 3alamaty sarswrrman dabney la print awa run naka awaste .
+    println(name)!! // lera aw dw 3alamaty sarswrrmana ka datna wata abe variable ka value y habe agar na yaksar awaste bot run naka .
     name="jwtiyar"
     println(name)
 }
