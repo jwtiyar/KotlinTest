ccb02047041946721dc484718d3c4b612b343bbd
jwtiyar <jwtiyar@gmail.com>
2017-07-29 21:00:37 +0300

    more

---
+++ src/SimpleIF.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>) {
+
+    println("jmareyek bnwse:")
+    var jmare:Int= readLine()!!.toInt()
+    if (jmare>1 && jmare<5){
+        //block of code
+        println("jmareket le newan 1 bo 5 daye ")
+    }
+
+    if (jmare>5){
+        println("jmareke le newan 5 w 1 nye")
+    }
+    println("lesht sagh be :)")
+}
