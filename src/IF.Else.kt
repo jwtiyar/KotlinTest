869f2c3050d1e226e3fef85bd3ce1dd0ee7ca4f2
jwtiyar <jwtiyar@gmail.com>
2017-07-29 21:45:55 +0300

    more

---
+++ src/IF.Else.kt
@@ -0,0 +1,18 @@
+fun main(args: Array<String>) {
+
+
+    println("tkaye derejeket bnwse : ")
+    var derejekt:Int?= readLine()!!.toInt()
+    if (derejekt!! >=45){
+        derejekt=derejekt+5
+        println("to darchwy derejeket brytye le $derejekt")
+
+        }else {
+        if (derejekt<45 && derejekt>=1){
+            println("to dernechwy , berdewam be qet nakewy :)")
+        }else{
+            println("to nehatwy bo xwendn gwllm ")
+        }
+    }
+    }
+
