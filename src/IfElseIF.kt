d0d8b4628f314dee0999b25fc214cf337f44ddb8
jwtiyar <jwtiyar@gmail.com>
2017-07-29 22:10:59 +0300

    more

---
+++ src/IfElseIF.kt
@@ -0,0 +1,18 @@
+fun main(args: Array<String>) {
+
+    println("derejeket bnwse: ")
+    val derejeket:Int= readLine()!!.toInt()
+    if (derejeket>=90 && derejeket<=100){
+        println("nayab")
+
+    }else if (derejeket>=80 && derejeket<90){
+        println("zor bashe")
+    }else if (derejeket>=70 && derejeket<80){
+        println("basha")
+    }else {
+        println("zor seqety")
+    }
+    // eger beto hemww else kan ladem bes IF bet heman anjama ballam compiling
+    // w debugging katy zor debat boye awa xera tre chwnke eger hemwwy if be checky hemwwy ekat
+    // enja arrwa bas else if yakjar ka shartakay qbwll krd etr shty tr nakat.
+}
