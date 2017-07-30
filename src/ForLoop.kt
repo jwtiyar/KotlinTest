b9d3b2dc8c6e6721a1eb7b393133a3b6a0ef5852
jwtiyar <jwtiyar@gmail.com>
2017-07-30 18:38:52 +0300

    forloop

---
+++ src/ForLoop.kt
@@ -0,0 +1,25 @@
+fun main(args: Array<String>) {
+    //forloop bo dwbare krdnaway eshekete wata chan jarek esheke dwbare dekatawa;
+    // bo nmwna agar btawe wshayak chan jare bwsretawa yaksar byr lawa dakayata penj jar bnwsy print
+    //ballam ama bo ay agar 100 jar bw chy akay? boya forloop bo am karana gwnjawa ba yak kode aykat .
+    for (nwsynaka in 1..10) {
+        println("chony")
+
+        //harwaha datwanyn pey bllen pyshan man da ka kam xtwa chy print aka .
+        println("znjyra: $nwsynaka")
+    }
+
+
+// ama zor grnge chwnka la babataka abe tebgay ,
+// lam nmwnay xwarawa chy krawa,peman wtwa la 1 bo 3 wata 3 jar am bashay dway xom print bka,
+// bashakay dway xoysh forloop e tra ka awysh peman wtwa 2 jar dwbarawa barawa,wata abe 3 jar,
+// 1,2 dwbara bbetawa awysh ka dawakary yakam xwl tawaw dakat enja dacheta xwly dway xoy,
+// anjam awaya ka aw chwa yakam hangaw ka yakjar print y kat das pe akat ballam chwnka dway,
+// xoy loop e tr haya kawata abe awa tawaw ka enja bche dwam jar print y katawa,
+// debug bka ley te agay ba jwany .
+    for (item1 in 1..3){
+        for (item2 in 1..2)
+
+        println(" jmary2:$item2")
+    }
+}
