43a81d9412979143921d15c55865d8bf46f2c897
jwtiyar <jwtiyar@gmail.com>
2017-08-03 18:02:10 +0300

    forloop

---
+++ src/WhileDoWhile.kt
@@ -0,0 +1,16 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+    // amysh zor la forloop dache.
+    var jmarekrdn = 1
+    while (jmarekrdn<5) { // lera peman wt jmarayak ka la 5 kamtr be .
+        println("chaky")
+        jmarekrdn=jmarekrdn+1 // lera ka anjamakay tawaw krd anja 1 y bo zyad aka w achetawa sarata.
+    }
+
+    do {  //agar hatw sharataka je baje nabw de bo era
+        println("chaky")
+        jmarekrdn=jmarekrdn+1
+    }while (jmarekrdn>5) // lam xtwa ka agarretawa 1 bo jmaraka zyad aka ka ama manay waya,
+    // baradawam dabe kodaka chwnka harchyak bene w 1 byxata sar la 5 gawra tra .
+}
