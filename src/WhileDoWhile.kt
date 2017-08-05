59a6a30004b480cec5e8fb710764a43e0091e870
jwtiyar <jwtiyar@gmail.com>
2017-08-05 18:45:56 +0300

    h

--- src/WhileDoWhile.kt
+++ src/WhileDoWhile.kt
@@ -1,5 +1,5 @@
 import java.util.*
-
+// le while shrtaka check aka enja tanfyzy akat ballam do while tanfyzy aka enja check y shrtaka akat
 fun main(args: Array<String>) {
     // amysh zor la forloop dache.
     var jmarekrdn = 1
@@ -7,10 +7,12 @@ fun main(args: Array<String>) {
         println("chaky")
         jmarekrdn=jmarekrdn+1 // lera ka anjamakay tawaw krd anja 1 y bo zyad aka w achetawa sarata.
     }
-
-    do {  //agar hatw sharataka je baje nabw de bo era
-        println("chaky")
+    jmarekrdn=200
+    do {
+        //agar hatw sharataka je baje nabw de bo era
         jmarekrdn=jmarekrdn+1
-    }while (jmarekrdn>5) // lam xtwa ka agarretawa 1 bo jmaraka zyad aka ka ama manay waya,
+    }while (jmarekrdn>5)
+        print("basha")
+     // lam xtwa ka agarretawa 1 bo jmaraka zyad aka ka ama manay waya,
     // baradawam dabe kodaka chwnka harchyak bene w 1 byxata sar la 5 gawra tra .
 }
